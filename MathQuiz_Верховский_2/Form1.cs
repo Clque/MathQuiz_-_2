0318f917b5f27a8a88952851a38c3797f2f456aa
t8965 <t89653196502@yandex.ru>
2023-09-13 15:59:45 +0300

    dsads

--- MathQuiz_Верховский_2/Form1.cs
+++ MathQuiz_Верховский_2/Form1.cs
@@ -22,6 +22,21 @@ namespace MathQuiz_Верховский_2
         int addend1;
         int addend2;
 
+        // These integer variables store the numbers 
+        // for the subtraction problem. 
+        int minuend;
+        int subtrahend;
+
+        // These integer variables store the numbers 
+        // for the multiplication problem. 
+        int multiplicand;
+        int multiplier;
+
+        // These integer variables store the numbers 
+        // for the division problem. 
+        int dividend;
+        int divisor;
+
         public void StartTheQuiz()
         {
             // Fill in the addition problem.
@@ -40,9 +55,31 @@ namespace MathQuiz_Верховский_2
             // This step makes sure its value is zero before
             // adding any values to it.
             sum.Value = 0;
+
+            // Fill in the subtraction problem.
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            // Fill in the multiplication problem.
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            // Fill in the division problem.
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
         }
 
-        public Form1()
+            public Form1()
         {
             InitializeComponent();
         }
